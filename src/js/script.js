80ca2c5e1f94551867a21cd6984b93c42652e75b
Vladyslav <vladmelnikov022@gmail.com>
2026-02-25 20:45:02 +0200

    new JS features

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,45 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+try {
+    new Swiper('.works__slider', {
+        slidesPerView: 1,
+        loop: true,
+        navigation: {
+            nextEl: ".icon-right-open",
+            prevEl: ".icon-left-open",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        breakpoints: {
+            // when window width is >= 1200px
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+            },
+            1920: {
+                spaceBetween: 5,
+                slidesPerView: 3,
+            },
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        modules: [Navigation, Pagination],
+    });
+
+} catch (e) {}
+
+
+// Styles
 import "purecss/build/grids-min.css";
 import "purecss/build/grids-responsive-min.css";
 import "/src/sass/style.scss";
+
 
